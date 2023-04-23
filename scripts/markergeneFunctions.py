c971de5e8f3f389e08d9f43f2f2730412ada0642
ThomasCZhang <112586835+ThomasCZhang@users.noreply.github.com>
2023-04-22 21:55:43 -0400

    Added a save_dir argument to plot_marker_genes so save directory can be chosen when calling function.

--- scripts/markergeneFunctions.py
+++ scripts/markergeneFunctions.py
@@ -9,6 +9,8 @@ Functions for extracting marker genes from clusters by pairwise comparison
 """
 import h5py
 import json
+import os
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -20,31 +22,29 @@ def get_gene_stats_by_cluster(data_path,
 	GEt statistics for each cluster and each gene.
 	Parameters:
 		data_path: str
-			 path of the .h5 file
+			path of the .h5 file
 		 cluster_labels: numpy array
-			 Cluster labels for each point
+			Cluster labels for each point
 		 clusters: list
-			 List of clusters
+			List of clusters
 	 Returns:
 		median_arr: (# of cluster, # of genes) numpy array
-			   Array of median values for each cluster and each gene
-	   mean_arr: (# of cluster, # of genes) numpy array
-			   Array of mean values for each cluster and each gene		 
-	
-	
+			Array of median values for each cluster and each gene
+		mean_arr: (# of cluster, # of genes) numpy array
+			Array of mean values for each cluster and each gene		 
 	"""
 
 	with h5py.File(data_path, "r") as f:
 		num_genes = f['df']['block0_values'].shape[1]
 	median_arr = np.zeros((len(clusters), num_genes))
 	mean_arr = np.zeros_like(median_arr)
-# 	var_arr = np.zeros_like(mean_arr)
+	# var_arr = np.zeros_like(mean_arr)
 	with h5py.File(data_path, "r") as f:
-	    for n in range(len(clusters)):
-		    indices = np.where(cluster_labels==clusters[n])
-		    cluster_arr = f['df']['block0_values'][indices]
-		    mean_arr[n, :] = np.mean(cluster_arr, axis=0)
-		    median_arr[n, :] = np.median(cluster_arr, axis=0)	
+		for n in range(len(clusters)):
+			indices = np.where(cluster_labels==clusters[n])
+			cluster_arr = f['df']['block0_values'][indices]
+			mean_arr[n, :] = np.mean(cluster_arr, axis=0)
+			median_arr[n, :] = np.median(cluster_arr, axis=0)	
 # 		var_arr[n, :] = np.var(cluster_arr, axis=0)
 		
 	return median_arr, mean_arr
@@ -73,12 +73,12 @@ def filter_genes_by_median(median_arr, mean_arr):
 	for n in range(mean_arr.shape[1]):
 		top_cluster = np.argmax(median_arr[:, n])
 		if mean_arr[top_cluster, n] >= 1:
-		    median_exp = median_arr[top_cluster, n]
-		    temp_median_arr = median_arr[:, n]
-		    temp_median_arr = np.delete(temp_median_arr, top_cluster, axis=0)
-		    if np.all(temp_median_arr*2 < median_exp):
-		        cluster_dict[top_cluster].append(n)
-		        marker_rank_dict[top_cluster].append(median_exp)
+			median_exp = median_arr[top_cluster, n]
+			temp_median_arr = median_arr[:, n]
+			temp_median_arr = np.delete(temp_median_arr, top_cluster, axis=0)
+			if np.all(temp_median_arr*2 < median_exp):
+				cluster_dict[top_cluster].append(n)
+				marker_rank_dict[top_cluster].append(median_exp)
 
 	#sort markers by fold change over mean
 	for n in cluster_dict.keys():
@@ -91,9 +91,9 @@ def filter_genes_by_median(median_arr, mean_arr):
 				
 	#save sorted arrays
 	with open('marker_genes.json', 'w') as f:
-	    json.dump(cluster_dict, f)
+		json.dump(cluster_dict, f)
 	with open('marker_genes_fc.json', 'w') as f:
-	    json.dump(marker_rank_dict, f)		
+		json.dump(marker_rank_dict, f)		
 		
 	return
 
@@ -101,6 +101,8 @@ def plot_marker_genes(data_path,
 					  clusters, 
 					  labels,
 					  n_genes:int=3,
+					  gene_list_path:str='./data/human_MTG_2018-06-14_genes-rows.csv',
+					  save_dir: str = None
 					  ):
 	
 	"""
@@ -120,6 +122,9 @@ def plot_marker_genes(data_path,
 	
 	"""
 	
+	df = pd.read_csv(gene_list_path, delimiter=',')
+	gene_names = df['gene'].to_list()
+	
 	with open('marker_genes.json') as f:
 		marker_genes = json.load(f)
 
@@ -134,15 +139,18 @@ def plot_marker_genes(data_path,
 		fig.supylabel(f'Gene expression', fontsize=20)
 		
 		with h5py.File(data_path, "r") as f:
-		    for m in range(len(clusters)):
-		        indices = np.where(labels == clusters[m])
-		        gene_matrix = f['df']['block0_values'][indices]
+			for m in range(len(clusters)):
+				indices = np.where(labels == clusters[m])
+				gene_matrix = f['df']['block0_values'][indices]	
+				for g in range(max_num):
+					gene = gene_list[g]
+					gene_data = gene_matrix[:, gene]
+					ax[g].violinplot(dataset=gene_data, positions=[m], showextrema=False)
+					ax[g].set_ylabel(gene_names[gene], fontsize=16)					
 
-		        for g in range(max_num):
-			        gene = gene_list[g]
-			        gene_data = gene_matrix[:, gene]
-			        ax[g].violinplot(dataset=gene_data, positions=[m], showextrema=False)
-			        ax[g].set_ylabel(gene, fontsize=16)					
-		
-		    fig.tight_layout()
-		    plt.savefig(f'marker_gene_{n}.jpg')
+			fig.tight_layout()
+			if save_dir is None:
+				save_path = f'marker_gene_{n}.jpg'
+			else:
+				save_path = os.path.join(save_dir, f'marker_gene_{n}.jpg')
+			fig.savefig(save_path)
